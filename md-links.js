34c8970a3bdee4fb645dbe3e5267b97c6e64877a
JaquelineRocio <jaquelineramosvargas@gmail.com>
2022-05-10 23:46:42 -0500

    Crear funcion convertir ruta relativa a absoluta. Close #4

--- md-links.js
+++ md-links.js
@@ -8,18 +8,4 @@ export const isAbsolutePath=(pathRoot)=>{
 export const convertToAbsolutePath = (pathRoot) =>{
     return resolve(pathRoot);
 }
-export const getPathFiles=(pathRoot)=>{
-  const fileNames = fs.readdirSync(pathRoot);
-  const arrFiles = fileNames.map(fileName => {
-                              const filePath = join(pathRoot, fileName);
-                              const statFile = fs.statSync(filePath);
-                              return (statFile.isDirectory())? getPathFilesSync(filePath): filePath;
-                            })
-  return arrFiles.flat();
-}
 
-export const isMdFile=(arrPaths)=>{
-  return arrPaths.filter(direction => path.extname(direction)== ".md");
-}
-
-console.log(isAbsolutePath(rootPath));
