58edf7bf2ac6b627b4c4f783a2c3849a2b67453a
JaquelineRocio <jaquelineramosvargas@gmail.com>
2022-05-10 23:49:41 -0500

    Implementar funcion listar archivos de carpetas anidadas. Close #1

--- md-links.js
+++ md-links.js
@@ -8,4 +8,16 @@ export const isAbsolutePath=(pathRoot)=>{
 export const convertToAbsolutePath = (pathRoot) =>{
     return resolve(pathRoot);
 }
+export const getPathFiles=(pathRoot)=>{
+    const fileNames = fs.readdirSync(pathRoot);
+    const arrFiles = fileNames.map(fileName => {
+                                const filePath = join(pathRoot, fileName);
+                                const statFile = fs.statSync(filePath);
+                                return (statFile.isDirectory())? getPathFilesSync(filePath): filePath;
+                              })
+    return arrFiles.flat();
+  }
+  
 
+  
+  console.log(isAbsolutePath(rootPath));
